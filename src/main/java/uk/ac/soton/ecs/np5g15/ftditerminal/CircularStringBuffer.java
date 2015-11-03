743e7c276db3f56d7cc8bb81de8565f2465d1c72
xpleria <illusion.webs@ymail.com>
2015-11-03 13:51:26 +0000

    Updated Copyright

--- src/main/java/uk/ac/soton/ecs/np5g15/ftditerminal/CircularStringBuffer.java
+++ src/main/java/uk/ac/soton/ecs/np5g15/ftditerminal/CircularStringBuffer.java
@@ -6,12 +6,8 @@
  * Redistribution and use with or without modification,
  * are permitted provided that the following conditions are met:
  *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
+ * o Redistributions of source code must retain the above copyright notice, this
+ *   condition and the following disclaimer.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
